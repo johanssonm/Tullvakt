65739b23ed873d357372165a507f79a4886538ad
Unknown <mattiasj@protonmail.com>
2018-05-19 13:21:43 +0200

    Börjar fila på nya variant

---
+++ Tullvakt/Rules.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Tullvakt
+{
+    public class Rules
+    {
+        
+        public static double Rule1_Rule2(Vehicle vehicle, double fee)
+        {
+            if (vehicle.Weight <= Toll.StandardWeightInKgs)
+            {
+                return Toll.LightWeightVehicleFee;
+            }
+
+            return Toll.HeavyWeightVehicleFee;
+
+        }
+
+        public static void DetermineNightToll(Vehicle vehicle)
+        {
+
+            TimeSpan startTollNightFee = new TimeSpan(StartNightFee);
+            TimeSpan endTollNightFee = new TimeSpan(EndNightFee);
+
+
+            while (true)
+            {
+                if (vehicle is Truck)
+                {
+                    break;
+                }
+
+                if (vehicle.PassageDay == DayOfWeek.Saturday ||
+                    vehicle.PassageDay == DayOfWeek.Sunday)
+                {
+                    break;
+                }
+
+                if (vehicle.PassageTime > startTollNightFee ||
+                    vehicle.PassageTime < endTollNightFee)
+                {
+                    vehicle.Fee = vehicle.Fee * FeeCalc.NightTollDiscount;
+                }
+
+                break;
+
+            }
+        }
+
+        public static void DetermineOvertimePay(Vehicle vehicle)
+        {
+
+            TimeSpan startTollNightFee = new TimeSpan(StartNightFee);
+            TimeSpan endTollNightFee = new TimeSpan(EndNightFee);
+
+
+            while (true)
+            {
+                if (vehicle is Truck)
+                {
+                    break;
+                }
+
+                if (vehicle.PassageDay == DayOfWeek.Saturday ||
+                    vehicle.PassageDay == DayOfWeek.Sunday)
+                {
+                    break;
+                }
+
+                if (vehicle.PassageTime > startTollNightFee ||
+                    vehicle.PassageTime < endTollNightFee)
+                {
+                    vehicle.Fee = vehicle.Fee * FeeCalc.NightTollDiscount;
+                }
+
+                break;
+
+            }
+        }
+
+
+    }
+
+}
---
+++ Tullvakt/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tullvakt
+{
+
+
+	public abstract class Vehicle
+	{
+
+		public int Weight { get; set; }
+		public bool EnviromentFriendly { get; set; }
+
+		public Vehicle()
+		{
+		}
+	}
+
+   
+	public class Truck : Vehicle
+    {
+    
+    }
+
+    public class PrivateCar : Vehicle
+    {
+
+
+    }
+
+    public class MotorCycle : Vehicle
+    {
+    
+
+    }
+
+
+}
